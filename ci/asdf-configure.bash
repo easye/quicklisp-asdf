97eb2ccc76046c1a75f0e1700cfd9c6eb33939a2
Mark Evenson <evenson.not.org@gmail.com>
2019-12-09 23:59:34 +0100

    ci: fix ASDF locations for Travis

--- ci/asdf-configure.bash
+++ ci/asdf-configure.bash
@@ -7,8 +7,13 @@ dest=$HOME/.config/common-lisp/source-registry.conf.d
 mkdir -p $dest
 
 # Locate the root of the source repository
-root=${TRAVIS_BUILD_DIR}
-root=$HOME/work/quicklisp-asdf
+if [[ -z ${TRAVIS_BUILD_DIR} ]]; then
+    # use my local conventions
+    root=$HOME/work/quicklisp-asdf
+else 
+    root=${TRAVIS_BUILD_DIR}
+fi
+
 
 # TODO figure out how to sanely address paths
 # TODO don't spam ASDF
